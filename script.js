82ca046cc6e6be0a61fdab048f8117e57dbae355
197naveen <naveenx@proton.me>
2023-01-09 22:08:39 +0530

    updatev2

--- script.js
+++
@@ -1,7 +0,0 @@
-function myfunc() {
-    var noOfFloors = document.getElementById("no-of-floors").value ;
-    var noOfLifts = document.getElementById("no-of-lifts").value ;
-    var newElem = "no of floors" + noOfFloors + and + "no of lifts" + noOfLifts;
-    var modElem =  document.getElementById("n-e")
-    modElem.innerHTML;
-}
